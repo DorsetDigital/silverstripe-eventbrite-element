3333bfb6a3be99dc6aa6f4d1b2b6fdc14c321f99
Tim Burt <tim@dorset-digital.net>
2019-04-15 17:01:37 +0100

    Add basic code

---
+++ src/DataObject/Event.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace DorsetDigital\SilverStripeEventBrite\DataObject;
+
+use SilverStripe\ORM\DataObject;
+
+class Event extends DataObject
+{
+    private static $table_name = 'DD_Event';
+    private static $singular_name = 'Event';
+    private static $plural_name = 'Events';
+
+    private static $db = [
+
+    ];
+}
---
+++ src/Dev/EventSync.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace DorsetDigital\SilverStripeEventBrite\Dev;
+
+use SilverStripe\Dev\BuildTask;
+
+class EventSync extends BuildTask
+{
+    public function run($request)
+    {
+        // TODO: Implement run() method.
+    }
+}
